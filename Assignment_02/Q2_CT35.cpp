552aa78ef874984ec80e7a8d26d244a8fa008994
Syed Wahaaj Ali <syedwahaajali2006@gmail.com>
2025-10-13 12:14:04 +0500

    Create Q2_CT35.cpp

---
+++ Assignment_02/Q2_CT35.cpp
@@ -0,0 +1,46 @@
+/*
+Assignment #2
+Question 2:
+Use a stack to reverse a given string.
+*/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+/*
+   Name: Syed Wahaaj Ali
+   Roll No: CT-24035
+   Section A - CSIT
+*/
+
+string reverseString(string str) {
+    stack<char> st;
+
+    // Push each character onto the stack
+    for (int i = 0; i < str.length(); i++) {
+        st.push(str[i]);
+    }
+
+    // Pop from stack to get reversed order
+    string reversed = "";
+    while (!st.empty()) {
+        reversed += st.top();
+        st.pop();
+    }
+
+    return reversed;
+}
+
+int main() {
+    string str;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    str reversed = reverseString(str);
+
+    cout << "Reversed string: " << reversed << endl;
+
+    return 0;
+}
