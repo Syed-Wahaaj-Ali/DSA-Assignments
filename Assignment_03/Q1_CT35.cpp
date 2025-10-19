90e118e5682194c1571afc15a5e4264cca1d9231
Syed Wahaaj Ali <syedwahaajali2006@gmail.com>
2025-10-19 15:49:34 +0500

    Added Question_01

---
+++ Assignment_03/Q1_CT35.cpp
@@ -0,0 +1,30 @@
+/*
+Assignment #3
+Question 1:
+Write a recursive function to compute the factorial of a non-negative integer n.
+*/
+
+#include <iostream>
+using namespace std;
+
+/*
+   Name: Syed Wahaaj Ali
+   Roll No: CT-24035
+   Section A - CSIT
+*/
+
+// Recursive function to calculate factorial
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1; // Base case
+    return n * factorial(n - 1); // Recursive call
+}
+
+int main() {
+    int n;
+    cout << "Enter a non-negative integer: ";
+    cin >> n;
+
+    cout << "Factorial of " << n << " is " << factorial(n) << endl;
+    return 0;
+}
